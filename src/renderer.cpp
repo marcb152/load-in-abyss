2fcb7a809e817b1f6af39d5d708e6680475a9f91
Marc <67200910+marcb152@users.noreply.github.com>
2025-04-14 13:53:33 +0200

    Added clang-format and initial Renderer class
    Added a .clang-format file to enforce consistent code style.

---
+++ src/renderer.cpp
@@ -0,0 +1,10 @@
+//
+// Created by marc on 14/04/25.
+//
+
+#include "renderer.hpp"
+
+namespace Abyss
+{
+
+} // Abyss
---
+++ src/renderer.hpp
@@ -0,0 +1,18 @@
+//
+// Created by marc on 14/04/25.
+//
+
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
+namespace Abyss
+{
+
+class renderer
+{
+
+};
+
+} // Abyss
+
+#endif //RENDERER_HPP
