82c8a1f93bc8664de04ad3bf664620ba61b6f6f2
Marc <67200910+marcb152@users.noreply.github.com>
2025-04-14 22:14:11 +0200

    Added empty box class for later implementation
    The goal is to extract geometry from renderer.cpp

---
+++ src/box.cpp
@@ -0,0 +1,8 @@
+//
+// Created by marc on 14/04/25.
+//
+
+#include "box.hpp"
+
+namespace Abyss {
+} // Abyss
---
+++ src/box.hpp
@@ -0,0 +1,16 @@
+//
+// Created by marc on 14/04/25.
+//
+
+#ifndef BOX_HPP
+#define BOX_HPP
+
+namespace Abyss {
+
+class Box {
+
+};
+
+} // Abyss
+
+#endif //BOX_HPP
