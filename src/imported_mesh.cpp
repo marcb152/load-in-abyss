72231d72e6c955af35004228facdeafefd155ebf
Marc <67200910+marcb152@users.noreply.github.com>
2025-04-19 22:27:31 +0200

    Created empty ImportedMesh class for assimp-loaded meshes

---
+++ src/imported_mesh.cpp
@@ -0,0 +1,10 @@
+//
+// Created by marc on 19/04/25.
+//
+
+#include "imported_mesh.hpp"
+
+namespace Abyss
+{
+
+} // Abyss
---
+++ src/imported_mesh.hpp
@@ -0,0 +1,16 @@
+//
+// Created by marc on 19/04/25.
+//
+
+#ifndef IMPORTED_MESH_HPP
+#define IMPORTED_MESH_HPP
+
+namespace Abyss
+{
+    class ImportedMesh
+    {
+
+    };
+} // Abyss
+
+#endif //IMPORTED_MESH_HPP
